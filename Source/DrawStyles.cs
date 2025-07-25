2fcb3eaa91325fb3b542f0631f391d390a2a2b19
Shaun McFall <shaun@shaunmcfall.com>
2025-07-25 12:33:52 -0400

    Remove unused parameter

--- Source/DrawStyles.cs
+++ Source/DrawStyles.cs
@@ -12,7 +12,7 @@ public class Filled : Verse.DrawStyle
     public override void Update(IntVec3 origin, IntVec3 target, List<IntVec3> buffer)
     {
         buffer.Clear();
-        buffer.AddRange(SunLamp.Filled(target, target));
+        buffer.AddRange(SunLamp.Filled(target));
     }
 }
 
@@ -21,6 +21,6 @@ public class Outline : Verse.DrawStyle
     public override void Update(IntVec3 origin, IntVec3 target, List<IntVec3> buffer)
     {
         buffer.Clear();
-        buffer.AddRange(SunLamp.Outline(target, target));
+        buffer.AddRange(SunLamp.Outline(target));
     }
 }
